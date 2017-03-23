91d4bad872c0bd24c72b2e9431d8a077993fba5c
Andres Jimenez <vjimenez@stud112>
2017-03-23 12:00:26 +0100

    asdf

--- matrix.py
+++ matrix.py
@@ -3,27 +3,39 @@ from copy import copy
 class Matrix(object):
 	def __init__(self, *args):
 		#Task1
+		self.rows= *args
+		e=0
+		i=0
+
+		i=len(self.rows)
+		e=len(args[0])
+		self.shape=(i,e)
+
 
 	def indices_generator(self):
 		#Task2
-
+		pass
 
 	def apply(self, fun, **kwargs):
 		#Task3
 		# must use inidices_generator (task2)
+		pass
 
 	def __add__(self, m):
 		#Task4
 		# must use apply (task3)
+		pass
 
 	@property
 	def transpose(self):
 		#Task5
 		# transpose the matrix
+		pass
 
 	def __str__(self):
 		#Task6
 		# return the string representation
+		pass
 
 """
 The output of those lines:
@@ -35,7 +47,7 @@ print m2
 m3= m1+m2
 print m3
 print m2
-print m1+3  
+print m1+3
 print m1.transpose
 
 Should be following:
